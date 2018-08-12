f060d4c6c3c22c42a40965293f3dd48baa9d249d
丸本聡 <sagaminumber3@hotmail.co.jp>
2018-08-12 15:55:23 +0900

    edit Capistrano Version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock 'Capistrano Version: 3.11.0 (Rake Version: 12.3.1)'
+lock '3.11.0'
 
 set :application, 'chat-space2'
 set :repo_url,  'git@github.com:marumimumemo/chat-space2.git'
