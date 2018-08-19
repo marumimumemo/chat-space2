2fd7fe1ac236fec460f6ec56b3393f5612991b9a
丸本聡 <sagaminumber3@hotmail.co.jp>
2018-08-19 17:25:23 +0900

    commit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,6 +5,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :fog
 
+  include CarrierWave::MiniMagick
+
+  process resize_to_fit: [400, 400]
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
   json.content @message.content
   json.name @message.user.name
-  json.image @message.image
+  json.image @message.image.url
   json.date @message.created_at.to_s(:datetime)
   json.id @message.id
